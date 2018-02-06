4b11cb8f2086c6df7baae6c655cd8234f2b40504
Ujjawal Sharma <uzzzval@gmail.com>
2018-02-05 22:26:17 -0500

    Update TCPDumpAnalyzer.py

--- TCPDumpAnalyzer.py
+++ TCPDumpAnalyzer.py
@@ -4,7 +4,6 @@ from subprocess import Popen, PIPE
 
 sudo_password = 'PASSWORD'
 command = 'tcpdump port 443'.split()
-
 p = Popen(['sudo', '-S'] + command, stdin=PIPE, stderr=PIPE,
           universal_newlines=True)
 sudo_prompt = p.communicate(sudo_password + '\n')[1]
